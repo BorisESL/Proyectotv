8b11f68c933c196d02154186ac8c8953b63d7e07
boris <boris.salgadolorca@gmail.com>
2024-09-02 17:52:43 -0400

    navbar transparencia, hero opacity

--- javascript/main.js
+++ javascript/main.js
@@ -1,17 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Selecciona los elementos necesarios
     const menuIcon = document.querySelector('.menu-icon');
     const navLinks = document.querySelector('.nav-links');
     const header = document.querySelector('.header');
 
-    // Añade un event listener al icono del menú
     menuIcon.addEventListener('click', function() {
-        // Alterna la clase 'active' en navLinks y menuIcon
         navLinks.classList.toggle('active');
         menuIcon.classList.toggle('active');
     });
 
-    // Cierra el menú si se hace clic fuera de él
     document.addEventListener('click', function(event) {
         const isClickInsideMenu = navLinks.contains(event.target) || menuIcon.contains(event.target);
         if (!isClickInsideMenu && navLinks.classList.contains('active')) {
@@ -20,7 +16,6 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    // Cierra el menú cuando se hace clic en un enlace (para navegación de una sola página)
     navLinks.querySelectorAll('a').forEach(link => {
         link.addEventListener('click', function() {
             navLinks.classList.remove('active');
@@ -36,4 +31,9 @@ document.addEventListener('DOMContentLoaded', function() {
             header.classList.remove('scrolled');
         }
     });
+
+    // Llamada inicial para establecer el estado correcto del header
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    }
 });

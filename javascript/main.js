9c172fe9762b781cf8b57294de4e23fb9c0a1331
boris <boris.salgadolorca@gmail.com>
2024-09-13 21:33:29 -0300

    orden hero

--- javascript/main.js
+++ javascript/main.js
@@ -1,14 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const menuIcon = document.querySelector('.menu-icon');
     const navLinks = document.querySelector('.nav-links');
     const header = document.querySelector('.header');
 
-    menuIcon.addEventListener('click', function() {
+    menuIcon.addEventListener('click', function () {
         navLinks.classList.toggle('active');
         menuIcon.classList.toggle('active');
     });
 
-    document.addEventListener('click', function(event) {
+    document.addEventListener('click', function (event) {
         const isClickInsideMenu = navLinks.contains(event.target) || menuIcon.contains(event.target);
         if (!isClickInsideMenu && navLinks.classList.contains('active')) {
             navLinks.classList.remove('active');
@@ -17,14 +17,14 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     navLinks.querySelectorAll('a').forEach(link => {
-        link.addEventListener('click', function() {
+        link.addEventListener('click', function () {
             navLinks.classList.remove('active');
             menuIcon.classList.remove('active');
         });
     });
 
     // Maneja el cambio de fondo del header al hacer scroll
-    window.addEventListener('scroll', function() {
+    window.addEventListener('scroll', function () {
         if (window.scrollY > 50) {
             header.classList.add('scrolled');
         } else {

0ef677af1e00928165442bfdbc26087fc1940838
boris <boris.salgadolorca@gmail.com>
2024-09-15 22:24:46 -0300

    slider

--- javascript/main.js
+++ javascript/main.js
@@ -38,4 +38,22 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
+const initSwiper = () => {
+    const swiper = new Swiper(".mySwiper", {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        coverflowEffect: {
+            rotate: 15,
+            stretch: 0,
+            depth: 300,
+            modifier: 1,
+            slideShadows: true,
+        },
+        loop: true,
+    });
+}
+
+initSwiper();
 
